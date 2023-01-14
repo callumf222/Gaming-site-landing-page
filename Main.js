1de0cfdd48743b42a9860e70219f323232b00e7a
Callumf222 <callumfry57@gmail.com>
2023-01-14 18:53:27 +0000

    navigation development

--- Main.js
+++ Main.js
@@ -16,3 +16,4 @@ function letter5(){
 function letter6(){
     document.getElementById("gameImage").style.backgroundImage="url(https://images.unsplash.com/photo-1583248352195-d3a8e766edf2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8ZmlyZXxlbnwwfDB8MHx8&auto=format&fit=crop&w=500&q=60)"    
 }
+
