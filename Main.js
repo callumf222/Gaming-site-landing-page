38c6d0a21f145d09abbe0cabf773cc2f2fec5700
Callumf222 <callumfry57@gmail.com>
2023-01-14 11:34:17 +0000

    image development

--- Main.js
+++ Main.js
@@ -1,3 +1,18 @@
 function letter1(){
-    document.getElementById("bd").style.backgroundImage="url(photo.jpg)"
+    document.getElementById("bd").style.backgroundImage="url(https://images.unsplash.com/photo-1583248352195-d3a8e766edf2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8ZmlyZXxlbnwwfDB8MHx8&auto=format&fit=crop&w=500&q=60)"    
+}
+function letter2(){
+    document.getElementById("bd").style.backgroundImage="url(https://images.unsplash.com/photo-1583248352195-d3a8e766edf2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8ZmlyZXxlbnwwfDB8MHx8&auto=format&fit=crop&w=500&q=60)"    
+}
+function letter3(){
+    document.getElementById("bd").style.backgroundImage="url(https://images.unsplash.com/photo-1583248352195-d3a8e766edf2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8ZmlyZXxlbnwwfDB8MHx8&auto=format&fit=crop&w=500&q=60)"    
+}
+function letter4(){
+    document.getElementById("bd").style.backgroundImage="url(https://images.unsplash.com/photo-1583248352195-d3a8e766edf2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8ZmlyZXxlbnwwfDB8MHx8&auto=format&fit=crop&w=500&q=60)"    
+}
+function letter5(){
+    document.getElementById("bd").style.backgroundImage="url(https://images.unsplash.com/photo-1583248352195-d3a8e766edf2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8ZmlyZXxlbnwwfDB8MHx8&auto=format&fit=crop&w=500&q=60)"    
+}
+function letter6(){
+    document.getElementById("bd").style.backgroundImage="url(https://images.unsplash.com/photo-1583248352195-d3a8e766edf2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8ZmlyZXxlbnwwfDB8MHx8&auto=format&fit=crop&w=500&q=60)"    
 }
